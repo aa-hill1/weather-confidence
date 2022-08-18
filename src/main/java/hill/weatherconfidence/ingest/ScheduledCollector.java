bbe962510b0dab57f3c9f6351b1f10b6242c5d83
arun <aa.hill1@outlook.com>
2022-08-18 18:10:33 +0100

    Spring Boot code

--- src/main/java/hill/weatherconfidence/ingest/ScheduledCollector.java
+++ src/main/java/hill/weatherconfidence/ingest/ScheduledCollector.java
@@ -2,17 +2,23 @@ package hill.weatherconfidence.ingest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Component
 public class ScheduledCollector {
 
     private static Logger log = LoggerFactory.getLogger(ScheduledCollector.class);
 
+    @Autowired
+    private WebClient webClient;
+
     @Scheduled(cron = "${ingest.cron}")
     public void collectLatest() {
         log.info("Collecting data");
+        webClient.get();
     }
 
 }
